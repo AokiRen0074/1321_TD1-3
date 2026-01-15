ac9bf7abd294d25e972d3af461d1371fd66e8bd6
ヤマト <k025g0093@g.neec.as.jp>
2026-01-15 16:26:27 +0900

    これでどうだー!!!!（迫真）

--- Router.cpp
+++ Router.cpp
@@ -21,7 +21,11 @@ void Router::InitRouter(int id, int mapData[kMapHeight][kMapWidth]) {
 		router_.bigRadius = 1300.0f;
 
 		break;
+	case 3://ルーターの効果範囲の設定
+		router_.radius = 600.0f;
+		router_.bigRadius = 1300.0f;
 
+		break;
 	default:
 		break;
 	}
