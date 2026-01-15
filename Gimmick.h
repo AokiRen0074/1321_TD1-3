beba8ce207b16aab41e97858d571c9cc5e852383
田谷 治幸 <nao.nao.y07@gmail.com>
2026-01-15 11:27:38 +0900

    gimmick作成途中

--- Gimmick.h
+++ Gimmick.h
@@ -2,11 +2,23 @@
 #include "Vector2.h"
 #include "Map.h"
 
-class Gimmick
-{
+class LiftGimmickBlock {
+public:
+	Vector2 pos_;
+	int linkId_; // LDtkで設定したIntegerの値
+	float speed_;
+	bool isActive_;
+	Vector2 startPos_;
+
+	LiftGimmickBlock();
+	~LiftGimmickBlock();
+
+	void Initialize();
+	void Update();
+	void Draw(Vector2 offset);
 };
 
-class LiftGimmickBlock {
+class LiftGimmickButton {
 public:
 
 };
